a855a1c5919284d4c251e951bbeeab5447dcb5ba
Sarvintowhid@gmail.com <sarvintowhid@gmail.com>
2022-09-19 02:13:11 -0500

    initial commit

---
+++ src/JavaPrac.java
@@ -0,0 +1,11 @@
+public class JavaPrac {
+
+    public static void main(String[] args) {
+        String name = "Sarvin Towhid";
+        int lent = name.length();
+        System.out.println(lent);
+
+        String lowCase = name.toLowerCase();
+        System.out.println(lowCase);
+    }
+}
